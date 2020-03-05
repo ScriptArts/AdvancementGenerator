132c874a69e60367cfdc576abdfe73812fa4c3e1
Narangelife <narange@scriptarts.jp>
2020-03-05 23:30:53 +0900

    泣いてる

---
+++ NarangeAdvancement/JSON_pop/json_window.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using OrangeNBT;
+using OrangeNBT.NBT;
+using OrangeNBT.NBT.IO;
+
+namespace NarangeAdvancement.JSON_pop
+{
+    /// <summary>
+    /// json_window.xaml の相互作用ロジック
+    /// </summary>
+    public partial class json_window : Window
+    {
+
+        private TagCompound root = new TagCompound();
+        
+
+        public json_window()
+        {
+            InitializeComponent();
+            List<TreeView> c = new List<TreeView>();
+            TreeView tr = new TreeView();
+            TreeView tra = new TreeView();
+            tr.Items.Add("A");
+            tra.Items.Add("ABC");
+            tr.Items.Add(tra);
+            c.Add(tr);
+            treeview_2.ItemsSource = c;
+        }
+
+        private void add_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void remove_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void done_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public TagCompound Root //外部からのroot呼び出し
+        {
+            get { return this.root; }
+        }
+
+    }
+}
---
+++ NarangeAdvancement/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OrangeNBT;
+using OrangeNBT.NBT;
+using OrangeNBT.NBT.IO;
+
+namespace NarangeAdvancement
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void json_button_1_Click(object sender, RoutedEventArgs e)
+        {
+            JSON_pop.json_window a = new JSON_pop.json_window();
+            a.ShowDialog();
+        }
+    }
+}
